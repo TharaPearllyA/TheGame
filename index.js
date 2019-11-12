d133a4d02e1b31a8ea8983d7fc996d768aad18b8
TharaPearllyA <tharapearllya@gmail.com>
2019-11-13 01:50:11 +0530

    added blog, contact and about

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import './style.css';
 const blogs = [
   {header: "The Begining",
   image : "url",
-  description: "loreum",
+  description: "Another argument this Value is used to tell the function to use this value when executing argument function",
   current : true
   },
   {header: "",
@@ -19,15 +19,22 @@ const blogs = [
   }
 ]
 
-const appDiv = document.getElementById('blog');
+const blog1 = document.getElementById('blog-current-1');
+const blog2 = document.getElementById('blog-current-2');
+const blog3 = document.getElementById('blog-current-3');
 
 const divs = [];
 blogs.forEach((blog, index) => {
     divs.push( 
-      "<div classname='blog'>" +
-      "<span>header:"  +
-           blog.header +
-      "</span>"+
-      "</div>" )
+      '<div class="inner-div"></div><p class="blog-header">'
+      +blog.header + '</p><p class="blog-descirption">'+
+      blog.description+'</p><div class="readmore-btn">Read More&nbsp; &gt;&gt;</div></div>')
   });
-console.log(divs)
+
+const index = blogs.findIndex(x => x.current === true)
+if(index){
+  blog1.innerHTML = divs[index];
+  blog2.innerHTML = divs[index+1];
+  blog3.innerHTML = divs[index+2];
+}
+console.log(divs[0])
