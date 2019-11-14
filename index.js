147513f22852759d4c4e32103409bd6802ebe911
TharaPearllyA <tharapearllya@gmail.com>
2019-11-14 21:39:42 +0530

    blog rectified

--- index.js
+++ index.js
@@ -2,20 +2,23 @@
 import './style.css';
 
 const blogs = [
-  {header: "The Begining",
-  image : "url",
-  description: "Another argument this Value is used to tell the function to use this value when executing argument function",
-  current : true
+  {
+    header: "The Begining",
+    image: "url",
+    description: "Another argument this Value is used to tell the function to use this value when executing argument function",
+    current: true
   },
-  {header: "",
-  image : "coming soon..",
-  description: "loreum",
-  current : false
+  {
+    header: "",
+    image: "coming soon..",
+    description: "loreum",
+    current: false
   },
-  {header: "",
-  image : "coming soon...",
-  description: "loreum",
-  current : false
+  {
+    header: "",
+    image: "coming soon...",
+    description: "loreum",
+    current: false
   }
 ]
 
@@ -25,16 +28,18 @@ const blog3 = document.getElementById('blog-current-3');
 
 const divs = [];
 blogs.forEach((blog, index) => {
-    divs.push( 
-      '<div class="inner-div"></div><p class="blog-header">'
-      +blog.header + '</p><p class="blog-descirption">'+
-      blog.description+'</p><div class="readmore-btn">Read More&nbsp; >></div></div>')
-  });
+  divs.push(
+    `<div class='inner-div'></div><p class='blog-header'>`
+    + blog.header +
+    `</p><p class='blog-descirption'>` +
+    blog.description
+    + `</p><div class='readmore-btn'>Read More&nbsp; >></div></div>`)
+});
 
-const index = blogs.findIndex(x => x.current === true)
-if(index){
+const index = blogs.findIndex(x => x.current === true);
+
+if (index != -1) {
   blog1.innerHTML = divs[index];
-  blog2.innerHTML = divs[index+1];
-  blog3.innerHTML = divs[index+2];
+  blog2.innerHTML = divs[index + 1];
+  blog3.innerHTML = divs[index + 2];
 }
-console.log(divs[0])
